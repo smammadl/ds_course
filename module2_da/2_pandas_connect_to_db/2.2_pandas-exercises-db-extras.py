bb5daec65bce0880a9b750e0782d463c75ee68ee
smammadl <sehriyarmemmedli@gmail.com>
2026-01-02 19:23:10 +0400

    db exercises correction

--- module2_da/2_pandas_connect_to_db/2.2_pandas-exercises-db-extras.py
+++ module2_da/2_pandas_connect_to_db/2.2_pandas-exercises-db-extras.py
@@ -1,6 +1,6 @@
 #----------------------------------------------------------------------------------------
 
-#-- Database: World
+#-- Database: world
 
 #-- Question 1: How many official languages are there?
 #-- Table: countrylanguage
@@ -14,55 +14,55 @@
 #----------------------------------------------------------------------------------------
 
 #-- How many female customers do we have from the state of Oregon (OR)?
-#-- Database: Store
-#-- Table: Customers
+#-- Database: store
+#-- Table: customers
 
 #-- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
-#-- Database: Store
-#-- Table: Customers
+#-- Database: store
+#-- Table: customers
 
 #-- Who between the ages of 30 and 50 has an income less than 50 000? (include 30 and 50 in the results)
-#-- Database: Store
-#-- Table: Customers
+#-- Database: store
+#-- Table: customers
 
 #-- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
-#-- Database: Store
-#-- Table: Customers
+#-- Database: store
+#-- Table: customers
 
 #----------------------------------------------------------------------------------------
 
 #-- Question: How many orders were made by customer 7888, 1082, 12808, 9623
-#-- DB: Store
+#-- DB: store
 #-- Table: orders
 
 #-- Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
-#-- DB: World
+#-- DB: world
 #-- Table: city
 
 #----------------------------------------------------------------------------------------
 
 #-- Who between the ages of 30 and 50 has an income less than 50 000? (include 30 and 50 in the results)
-#-- DB: Store
-#-- Table: Customers
+#-- DB: store
+#-- Table: customers
 
 #-- What is the average income between the ages of 20 and 50? (Including 20 and 50)
-#-- DB: Store
-#-- Table: Customers
+#-- DB: store
+#-- Table: customers
 
 #----------------------------------------------------------------------------------------
 
 #--Question: Can I get a list of distinct life expectancy ages (Make sure there are no nulls)
-#--DB: World
+#--DB: world
 #--Table: country
 
 #----------------------------------------------------------------------------------------
 
 #--Question: How many people's zipcode have a 2 in it?.
-#--DB: Store
+#--DB: store
 #--Table: customers
 
 #--Question: How many people's zipcode start with 2 with the 3rd character being a 1.
-#--DB: Store
+#--DB: store
 #--Table: customers
 
 #----------------------------------------------------------------------------------------
@@ -70,47 +70,47 @@
 #--Question: Select people either under 30 or over 50 with an income above 50000
 #--Include people that are 50
 #--that are from either Japan or Australia
-#--DB: Store
-#--Table: Customers
+#--DB: store
+#--Table: customers
 
 #-- Question: What was our total sales in June of 2004 for orders over 100 dollars?
-#-- DB: Store
-#-- Table: Orders
+#-- DB: store
+#-- Table: orders
 
 #----------------------------------------------------------------------------------------
 
 #-- Question: How many orders were made in January 2004?
-#-- DB: Store
+#-- DB: store
 #-- Table: orders
 
 #----------------------------------------------------------------------------------------
 
 #-- Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state ordered by orderid
-#-- DB: Store
+#-- DB: store
 #-- Table: orders
 
 #-- Question: Show me the inventory for each product
-#-- DB: Store
+#-- DB: store
 #-- Table: products
 
 #----------------------------------------------------------------------------------------
 
 #-- Find the average price for each category and then subtract the item’s price from its category’s price 
-#-- Database: Store
+#-- Database: store
 
 #-- Find the percentage of the world's population that lives on each continent.
-#-- Database: World
-#-- Table: Country
+#-- Database: world
+#-- Table: country
 
 #-- Show the population per continent
-#-- Database: World
-#-- Table: Country
+#-- Database: world
+#-- Table: country
 
 #-- To the previous query add on the ability to calculate the percentage of the world population
 #-- What that means is that you will divide the population of that continent by the total population and multiply by 100 to get a percentage.
 #-- Make sure you convert the population numbers to float using `population::float` otherwise you may see zero pop up
-#-- Database: World
-#-- Table: Country
+#-- Database: world
+#-- Table: country
 
 #----------------------------------------------------------------------------------------
 
@@ -121,16 +121,16 @@
 #-- Create a case statement that's named "price class" where if a product is over 20 dollars you show 'expensive'
 #-- if it's between 10 and 20 you show 'average' 
 #-- and of is lower than or equal to 10 you show 'cheap'.
-#-- Database: Store
+#-- Database: store
 #-- Table: products
 
 #-- Calculate the total area of countries that have a population of over 50 million 
-#-- DB: World
-#-- Table: Countries
+#-- DB: world
+#-- Table: countries
 
 #----------------------------------------------------------------------------------------
 
 #-- Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 #-- ordered by orderid
-#-- DB: Store
+#-- DB: store
 #-- Table: orders
